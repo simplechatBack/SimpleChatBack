7c51dacc2eb8669c162afb79c2c8997204c4c160
ParkSoyoung0125 <hellene125@gmail.com>
2026-03-01 22:30:47 +0900

    first simplechat push

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM eclipse-temurin:17-jdk AS build
+WORKDIR /app
+COPY . .
+RUN ./gradlew clean bootJar -x test
+
+FROM eclipse-temurin:17-jre
+WORKDIR /app
+COPY --from=build /app/build/libs/*.jar app.jar
+EXPOSE 9100
+ENTRYPOINT ["java","-jar","/app/app.jar"]
---
+++ src/main/java/com/example/simplechat/SimplechatApplication.java
@@ -0,0 +1,15 @@
+package com.example.simplechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplechatApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(SimplechatApplication.class, args);
+        System.out.println("Hello word!");
+	}
+
+}
